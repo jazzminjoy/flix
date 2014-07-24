4d37bd56899fb92b1f302619f4aa2294910601be
Michele Cannito <tigerrosere@gmail.com>
2014-07-24 01:09:45 -0700

    Original commit. Set up to use rspec and capybara gems (but not in production).

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flix::Application.config.secret_key_base = '338528c6fca17712c4d6cf23108da64a1d8e0dd698ca02420ba06bdae4ceb2af73ec5cb08fef42ce2a259366e30c2491ff6c039f412cf4d80cfb9a3d511dc92f'
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Flix::Application.routes.draw do
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+  root 'movies#index'
+
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
+  get "movies" => "movies#index"
+
+end
